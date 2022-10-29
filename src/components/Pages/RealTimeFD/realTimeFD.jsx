285ca7d72f49849e092415be0c92d0f5a6cb3577
Rohit Duhan <rohitduhan2301@gmail.com>
2022-10-29 19:29:46 +0530

    tensorflow, image Detection - Working

--- src/components/Pages/RealTimeFD/realTimeFD.jsx
+++ src/components/Pages/RealTimeFD/realTimeFD.jsx
@@ -1,27 +1,42 @@
 import React, { useRef } from "react";
 import { useEffect, useState } from "react";
-import * as tf from "@tensorflow/tfjs";
 import * as faceapi from "face-api.js";
-// import * as canvas from "canvas";
-import demoImage from "../../../assets/images/demoImage.jpg";
+import * as tf from "@tensorflow/tfjs";
+import demoImage from "../../../assets/images/download.jpg";
 import { HomeGetStartedBtn } from "../../../utils/buttons";
+import * as canvas from "canvas";
+import Webcam from "react-webcam";
 
 const RealTimeFD = () => {
   const imgRef = useRef();
   const canvaRef = useRef();
 
   const handleDetections = async () => {
-    const detections = await faceapi.detectAllFaces(imgRef);
-    const detections2 = await faceapi.detectAllFaces(
-      imgRef,
-      new faceapi.TinyFaceDetectorOptions()
-    );
-    console.log(detections2);
+    const detections = await faceapi
+      .detectAllFaces(imgRef.current, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks()
+      .withFaceExpressions();
+
+    canvaRef.current.innerHtml = faceapi.createCanvasFromMedia(imgRef.current);
+    faceapi.matchDimensions(canvaRef.current, {
+      width: 800,
+      height: 450,
+    });
+    const resized = faceapi.resizeResults(detections, {
+      width: 800,
+      height: 450,
+    });
+    faceapi.draw.drawDetections(canvaRef.current, resized);
   };
 
   useEffect(() => {
     const loadModels = () => {
-      Promise.allSettled([faceapi.nets.tinyFaceDetector.loadFromUri("/models")])
+      Promise.allSettled([
+        faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+        faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+        faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+        faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+      ])
         .then((values) => {
           console.log(values);
           handleDetections();
@@ -36,18 +51,23 @@ const RealTimeFD = () => {
 
   return (
     <div className="relative mt-[-1.5rem] w-full h-screen bgBlue flex flex-col items-center justify-center gap-y-14 py-12">
-      <div
-        className="relative w-[45%] h-3/4 flex items-between justify-center"
-        ref={canvaRef}
-      >
-        {/* <Webcam audio={false} className="" /> */}
+      <div className="relative w-[45%] h-3/4 flex items-between justify-center">
+        {/* <Webcam
+          audio={false}
+          ref={imgRef}
+          className="w-[800px] h-[450px] relative"
+        /> */}
         <img
+          crossOrigin="anonymous"
           src={demoImage}
           alt="demoImage"
           ref={imgRef}
-          className="w-full h-full relative"
+          className="w-[800px] h-[450px] relative object-contain"
         ></img>
-        <canvas className="w-full h-full z-[-10] absolute"></canvas>
+        <canvas
+          className="w-[800px] h-[450px] z-[10] absolute top-[0px] left-[0px]"
+          ref={canvaRef}
+        ></canvas>
       </div>
       <div className="flex items-between justify-center gap-x-40">
         <div className="">{<HomeGetStartedBtn data={"Start Video"} />}</div>
