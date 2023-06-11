bb8148042578696ffc63aa392ceaa3ead9dd2ccf
Rohit Duhan <rohitduhan2301@gmail.com>
2023-06-12 00:02:36 +0530

    updated firebase file

--- src/firebase.js
+++ src/firebase.js
@@ -5,22 +5,26 @@ import axios from "axios";
 let firebaseConfig = {};
 let app = {},
   auth = {};
-
-axios.get(`${process.env.REACT_APP_PORT + "/apiKeys"}`).then((res) => {
-  firebaseConfig = {
-    apiKey: res.data.FIREBASE_API_KEY,
-    authDomain: res.data.FIREBASE_AUTH_DOMAIN,
-    projectId: res.data.FIREBASE_PROJECT_ID,
-    storageBucket: res.data.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: res.data.FIREBASE_MESSAGING_SENDER_ID,
-    appId: res.data.FIREBASE_APP_ID,
-    measurementId: res.data.FIREBASE_MEASUREMENT_ID,
-  };
-  app = initializeApp(firebaseConfig);
-  auth = getAuth(app);
-  console.log("bw", auth, firebaseConfig);
-});
+// FIREBASE_API_KEY = "AIzaSyBkRAZOFOKgijwo-uQav60_O0DvdRp2XpQ";
+// FIREBASE_AUTH_DOMAIN = "vizeye-44544.firebaseapp.com";
+// FIREBASE_PROJECT_ID = "vizeye-44544";
+// FIREBASE_STORAGE_BUCKET = "vizeye-44544.appspot.com";
+// FIREBASE_MESSAGING_SENDER_ID = "78671939730";
+// FIREBASE_APP_ID = "1:78671939730:web:227a1721b5be9c7ef2fe4b";
+// FIREBASE_MEASUREMENT_ID = "G-9D9QE7D84Z";
+firebaseConfig = {
+  apiKey: "AIzaSyBkRAZOFOKgijwo-uQav60_O0DvdRp2XpQ",
+  authDomain: "vizeye-44544.firebaseapp.com",
+  projectId: "vizeye-44544",
+  storageBucket: "vizeye-44544.appspot.com",
+  messagingSenderId: "78671939730",
+  appId: "1:78671939730:web:227a1721b5be9c7ef2fe4b",
+  measurementId: "G-9D9QE7D84Z",
+};
+app = initializeApp(firebaseConfig);
+auth = getAuth(app);
+// console.log("bw", auth, firebaseConfig);
+// axios.get(`${process.env.REACT_APP_PORT + "/apiKeys"}`).then((res) => {});
 // Initialize Firebase Authentication and get a reference to the service
-console.log(auth);
 export { auth };
 // Initialize the FirebaseUI Widget using Firebase.
